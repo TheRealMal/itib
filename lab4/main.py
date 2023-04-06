703e60175df81826cc5b9c92a82a6c5e6340085c
therealmal <44868259+TheRealMal@users.noreply.github.com>
2023-04-06 23:19:02 +0300

    Update main.py

--- lab4/main.py
+++ lab4/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def generate_sets(n) -> list:
     from itertools import product as pr
@@ -8,11 +9,17 @@ class NNet:
     def __init__(self, n = 0.3) -> None:
         self.__learning_rate      = n                         # Норма обучения
         self.__k                  = 0                         # Эпохи
-        self.__center_coords      = self.find_center_coords() # Скрытых RBF-Нейронов
+        self.__center_coords      = self.__find_center_coords() # Скрытых RBF-Нейронов
         self.__J                  = len(self.__center_coords)
         self.__weights            = np.zeros(self.__J + 1)    # Синаптические веса
+        self.__plot               = []                        # Для построения графика
 
-    def find_center_coords(self) -> int:
+    def __clear(self) -> None:
+        self.__k                  = 0
+        self.__weights            = np.zeros(self.__J + 1)
+        self.__plot               = []
+
+    def __find_center_coords(self) -> int:
         J0, J1 = [], []
         for _ in generate_sets(4):
             if self.logic_function(_):
@@ -23,7 +30,7 @@ class NNet:
             return J0
         return J1
 
-    def logic_function(self, v) -> bool:
+    def logic_function(self, v):
         # 8 Variant
         return (v[0] or v[1] or v[3]) and v[2]
         # Example
@@ -71,27 +78,38 @@ class NNet:
     def get_ham_dist(self, learning_selection, current_selection) -> float:
         target_values = []
         for _ in learning_selection:
-            target_values.append(self.logic_function(_))
+            target_values.append(int(self.logic_function(_)))
         return np.count_nonzero(np.array(current_selection) != np.array(target_values))
 
-    def build_RBF_model(self, learning_selection):
+    def build_RBF_model(self, learning_selection, act_fn=0):
+        self.__clear()
         while True:
             out_vector = [] # Выходной вектор текущей эпохи
             for x in learning_selection:
-                net = self.net(x) 
-                y = self.activation_func(net, 0) # Выбрать функцию активации (Второй аргумент)
+                net = self.net(x)
+                # np.rint() Округляет результат (0 или 1), иначе зацикливается
+                # .astype(int) преобразует в int число для красоты вывода (Чтобы не было 1.0/0.0)
+                y = (np.rint(self.activation_func(net, act_fn))).astype(int) # Выбрать функцию активации (Второй аргумент)
                 out_vector.append(y)
                 error = self.logic_function(x) - y
                 phi_arr = [1] + [self.phi_function(x, c) for c in self.__center_coords]
                 delta = self.__learning_rate * error * np.array(phi_arr)
-                self.__weights += np.round(delta, 3)
+                self.__weights += delta
             ham_dist = self.get_ham_dist(learning_selection, out_vector)
             self.__print_epoch(out_vector, ham_dist, int(ham_dist == 0))
+            self.__plot.append(ham_dist)
             self.__k += 1
 
             if ham_dist == 0:
+                self.__generate_plot()
                 break
 
+    def __generate_plot(self) -> None:
+        plt.plot(self.__plot, "ko-")
+        plt.ylabel("Ошибка")
+        plt.xlabel("Эпоха")
+        plt.grid(True)
+        plt.show()
 
     def __print_epoch(self, out_vector, E, ifEnd=0) -> None:
         print("~" * 16)
@@ -108,14 +126,17 @@ class NNet:
 
 
 def main():
-    tmp = NNet(0.3)
-    tmp.build_RBF_model(generate_sets(4))
     # 8 Variant
-    #tmp.build_RBF_model([[0,0,0,0],[0,0,1,0],[0,1,1,0],[1,0,1,1],[1,1,0,1]])
+    net1 = NNet(0.3)
+    net1.build_RBF_model([[0,0,0,0],[0,0,1,0],[0,1,1,0],[1,0,1,1],[1,1,0,1]], 0)
+    net1.build_RBF_model([[0,0,0,0],[0,0,1,0],[0,1,1,0],[1,0,1,1],[1,1,0,1]], 3)
+
+    # Полный набор, пороговая функция
+    #tmp.build_RBF_model(generate_sets(4), 0)
     # Example
-    #tmp.build_RBF_model([[0,0,0,1],[0,1,1,1],[1,0,1,0],[1,0,1,1],[1,1,1,0]])
+    #tmp.build_RBF_model([[0,0,0,1],[0,1,1,1],[1,0,1,0],[1,0,1,1],[1,1,1,0]], 0)
     # Ram
-    #tmp.build_RBF_model([[0,0,1,0],[0,1,0,1]])
+    #tmp.build_RBF_model([[0,0,1,0],[0,1,0,1]], 2)
 
 if __name__ == "__main__":
     main()
